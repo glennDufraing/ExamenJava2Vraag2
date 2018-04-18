e150a8254874bc3b32bbf378cda902b1930da180
glennDufraing <32563734+glennDufraing@users.noreply.github.com>
2018-04-18 16:23:29 +0200

    oefening klaar
    inc.testresultaten

--- SPRING-EXAMEN-JAVA2-VRAAG2-START/src/main/java/edu/ap/spring/Exam.java
+++ SPRING-EXAMEN-JAVA2-VRAAG2-START/src/main/java/edu/ap/spring/Exam.java
@@ -1,7 +1,10 @@
 package edu.ap.spring;
 
 import java.awt.Point;
+import java.lang.reflect.Array;
 import java.util.*;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,22 +13,50 @@ public class Exam {
 	// Maak gebruik van lambdas en streams om een array met alle
 	// priemgetallen terug te geven
 	// 2 punten
+	
 	public int[] getPrimes(int[] numbers) {
+		int count = 0;
+	List<Integer>lijst=new ArrayList<>();
+		for(int n:numbers) {
+			lijst.add(n);
+		}
+		lijst = lijst.stream().filter(l->isPrime(l)).collect(Collectors.toList());
+		
+		int[]returnVal = new int[lijst.size()];
+		for(int n:lijst) {
+			returnVal[count] = n;
+			count++;
+		}
+		return returnVal;
+		
 		
 	}
-	
+	private boolean isPrime(int n) {
+	    for(int i=2;i<n;i++) {
+	        if(n%i==0)
+	            return false;
+	    }
+	    return true;
+	}
 	// Maak gebruik van lambdas en streams alle lowercase characters
 	// te tellen in de gegeven string
 	// 1 punt
 	public int countLowercaseCharacters(String string) {
-        
+		int aantal = (int) string.chars().filter(l-> Character.toUpperCase(l) != l).count();
+		
+
+        return aantal;
 	}
 	
 	// Maak gebruik van lambdas en streams om de som van alle
 	// x-coordinaten uit de lijst van points te berekenen
 	// 1 punt
+	
 	public int sumOfX(List<Point> points) {
+		List<Double> lijst=  points.stream().map(l ->l.getX()).collect(Collectors.toList());
 		
+		int sum =(int) lijst.stream().mapToDouble(Double::intValue).sum();
+		return sum;
 	}
 	
 	// Maak gebruik van lambdas en streams om een comma-separated
@@ -34,5 +65,10 @@ public class Exam {
 	// 2 punten
 	public String getXOverTwo(List<Point> points) {
 		
+		String lijst= points.stream().filter(l ->l.getX() >=2).map(l->String.valueOf(l.getX())).collect(Collectors.joining(","));
+		
+		lijst = lijst.replaceAll(".0", "");
+		 return lijst;
 	}
+	
 }
--- SPRING-EXAMEN-JAVA2-VRAAG2-START/src/main/java/edu/ap/spring/ExamApplication.java
+++ SPRING-EXAMEN-JAVA2-VRAAG2-START/src/main/java/edu/ap/spring/ExamApplication.java
@@ -1,5 +1,6 @@
 package edu.ap.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,10 +9,18 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ExamApplication {
+	Exam exam;
 	
+	@Autowired
+	public void setExam(Exam exam) {
+		this.exam = exam;
+	}
+
 	@Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return (args) -> {};
+		return (args) -> {
+		
+		};
     }
 	
 	public static void main(String[] args) {
