c74e83232a8340da5447313c3b36848371c1156b
glennDufraing <32563734+glennDufraing@users.noreply.github.com>
2018-04-18 14:34:38 +0200

    Initial commit

---
+++ SPRING-EXAMEN-JAVA2-VRAAG2-START/src/main/java/edu/ap/spring/Exam.java
@@ -0,0 +1,38 @@
+package edu.ap.spring;
+
+import java.awt.Point;
+import java.util.*;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Exam {
+	
+	// Maak gebruik van lambdas en streams om een array met alle
+	// priemgetallen terug te geven
+	// 2 punten
+	public int[] getPrimes(int[] numbers) {
+		
+	}
+	
+	// Maak gebruik van lambdas en streams alle lowercase characters
+	// te tellen in de gegeven string
+	// 1 punt
+	public int countLowercaseCharacters(String string) {
+        
+	}
+	
+	// Maak gebruik van lambdas en streams om de som van alle
+	// x-coordinaten uit de lijst van points te berekenen
+	// 1 punt
+	public int sumOfX(List<Point> points) {
+		
+	}
+	
+	// Maak gebruik van lambdas en streams om een comma-separated
+	// string te maken die alle x-coordinaten bevat die groter of gelijk 
+	// zijn aan twee
+	// 2 punten
+	public String getXOverTwo(List<Point> points) {
+		
+	}
+}
